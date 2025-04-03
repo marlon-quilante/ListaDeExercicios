a79b74deb4525afb80646fb85d72c426bd091659
Marlon Quilante <marlonquilante@gmail.com>
2025-04-03 20:26:35 -0300

    Feito o exercício 06

---
+++ ListaDeExercicios.Exercicio06/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ListaDeExercicios.Exercicio06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a nota 1 do aluno: ");
+            decimal nota1 = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Digite o peso da nota 1: ");
+            decimal pesoNota1 = decimal.Parse(Console.ReadLine());
+
+            Console.Write("\nDigite a nota 2 do aluno: ");
+            decimal nota2 = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Digite o peso da nota 2: ");
+            decimal pesoNota2 = decimal.Parse(Console.ReadLine());
+
+            decimal mediaAluno = ((nota1 * pesoNota1) + (nota2 * pesoNota2)) / (pesoNota1 + pesoNota2);
+
+            Console.Write($"\nMédia do aluno: {mediaAluno.ToString("F2")}");
+            Console.ReadLine();
+        }
+    }
+}
