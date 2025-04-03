2371c94943edccafca4caec3398d1f5213ae0f88
Marlon Quilante <marlonquilante@gmail.com>
2025-04-03 19:44:10 -0300

    Add project files.

---
+++ ListaDeExercicios.Exercicio01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ListaDeExercicios.Exercicio01
+{
+    internal class Program
+    {
+        //Crie um programa para calcular o volume de uma caixa retangular
+        //VOLUME = COMPRIMENTO * LARGURA * ALTURA
+        static void Main(string[] args)
+        {
+            #region input de dados
+            Console.Write("Digite o comprimento da caixa: ");
+            decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Digite a largura da caixa: ");
+            decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Digite a altura da caixa: ");
+            decimal altura = Convert.ToDecimal(Console.ReadLine());
+            #endregion
+
+            #region processamento
+            decimal volume = comprimento * largura * altura;
+            #endregion
+
+            #region output do resultado
+            Console.Write($"Volume da caixa: {volume}");
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
---
+++ ListaDeExercicios.Exercicio02/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ListaDeExercicios.Exercicio02
+{
+    internal class Program
+    {
+        //Crie um programa para calcular o volume de um Cilindro
+        // V = pi * r² * a
+        static void Main(string[] args)
+        {
+            //input de dados
+            Console.Write("Digite o raio do cilindro: ");
+            double raio = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a altura do cilindro: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            //processamento de dados
+            double volume = Math.PI * Math.Pow(raio, 2) * altura;
+
+            //output do resultado
+            Console.Write($"Volume do cilindro: {volume}");
+
+            Console.ReadLine();
+        }
+    }
+}
