9d31cee9ebef9a4ff60af03ced1f280b633ec9eb
Marlon Quilante <marlonquilante@gmail.com>
2025-04-03 20:08:44 -0300

    Feito o exercício 04

---
+++ ListaDeExercicios.Exercicio04/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ListaDeExercicios.Exercicio04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a temperatura em graus Celsius: ");
+            double tempCelsius = double.Parse(Console.ReadLine());
+
+            double tempFahrenheit = (tempCelsius * 1.8) + 32;
+
+            Console.Write($"\nTemperatura em Fahrenheit: {tempFahrenheit.ToString("F2")}");
+            Console.ReadLine();
+        }
+    }
+}
