710e448acac58612c6e096bdd4f8423a74241ab6
Marlon Quilante <marlonquilante@gmail.com>
2025-04-03 20:19:45 -0300

    Feito o exercício 05

---
+++ ListaDeExercicios.Exercicio05/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ListaDeExercicios.Exercicio05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o salário base do vendedor: ");
+            decimal salarioBase = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Digite o total de vendas do vendedor: ");
+            decimal totalVendas = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Digite o percentual de comissão: ");
+            decimal percentualComissao = decimal.Parse(Console.ReadLine());
+
+            decimal comissao = totalVendas * (percentualComissao / 100);
+            decimal salarioFinal = salarioBase + comissao;
+
+            Console.Write($"\nSalário do vendedor: R$ {salarioFinal.ToString("F2")}");
+            Console.ReadLine();
+        }
+    }
+}
