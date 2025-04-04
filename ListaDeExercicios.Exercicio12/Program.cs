9281e3d663affb1c327affd3a121704a2b235685
Marlon Quilante <marlonquilante@gmail.com>
2025-04-03 21:30:04 -0300

    Feito o exercício 12

---
+++ ListaDeExercicios.Exercicio12/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ListaDeExercicios.Exercicio12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int numero = 100; numero <= 200; numero++)
+            {
+                if (numero % 2 != 0)
+                {
+                    Console.WriteLine(numero);
+                }
+            }
+        }
+    }
+}
