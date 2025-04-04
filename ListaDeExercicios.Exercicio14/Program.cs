c105e4d51e0d7cbe5ed5a1bda9c9f9d282371443
Marlon Quilante <marlonquilante@gmail.com>
2025-04-04 00:16:17 -0300

    Feito o exercício 14

---
+++ ListaDeExercicios.Exercicio14/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ListaDeExercicios.Exercicio14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+            int[] aux = new int[numero];
+            int numeroFibonacci = 0;
+
+            for (int i = 0; i < numero; i++)
+            {
+                if (i > 1)
+                {
+                    if (numeroFibonacci >= numero)
+                    {
+                        break;
+                    }
+
+                    aux[i] = i;
+                    numeroFibonacci = aux[i - 1] + aux[i - 2];
+                    Console.WriteLine(numeroFibonacci);
+                    aux[i] = numeroFibonacci;
+                }
+                else
+                {
+                    if (numeroFibonacci >= 30)
+                    {
+                        break;
+                    }
+
+                    aux[i] = i;
+                    numeroFibonacci = aux[i];
+                    Console.WriteLine(numeroFibonacci);
+                    aux[i] = numeroFibonacci;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
